691abdd201d92b5956ef67a34c81b5c95846e3e1
sanaullah963 <allhera963@gmail.com>
2024-03-29 20:10:03 +0600

    profile modifaid

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -5,7 +5,7 @@ import ProductDetailHeadding from "../product/ProductDetailHeadding";
 function Profile() {
   return (
     <main>
-      <Container className={' '}>
+      <Container className={" "}>
         <div className="border-[4px] max-w-sm  sm:max-w-md md:max-w-[820px] mx-auto rounded-md p-1 sm:p-2 md:py-5 shadow-lg shadow-gray-400 my-10">
           {/* inner border */}
           <div className="flex flex-col md:flex-row gap-2 md:gap-5 mx-auto mb-8">
@@ -15,7 +15,9 @@ function Profile() {
                 <div className="h-10 w-16 sm:h-16 sm:w-24 rounded-3xl bg-green-200 text-green-700 flex justify-center items-center font-bold text-xl sm:text-3xl shadow-md shadow-green-500">
                   25
                 </div>
-                <p className="capitalize text-sm sm:text-lg">Products ordered</p>
+                <p className="capitalize text-sm sm:text-lg">
+                  Products ordered
+                </p>
               </div>
               {/* cart counting */}
               <div className=" border-2 border-blue-400 rounded-md flex items-center justify-center flex-col gap-2 py-6">
@@ -29,13 +31,20 @@ function Profile() {
             <div className=" flex-1 border-2 border-gray-400 bg-gray-100 rounded-md px-2 sm:px-3 py-2">
               <div className="">
                 <div className="flex justify-between items-center mb-3">
-                  <p className="capitalize text-lg sm:text-2xl">Shipping Address</p>
+                  <p className="capitalize text-lg sm:text-2xl">
+                    Shipping Address
+                  </p>
                   <span className="w-[1px] block h-[18px] bg-black"></span>
-                  <button className="capitalize flex items-center text-blue-600 gap-[2px]"><GoPlus/> add new</button>
+                  <button className="capitalize flex items-center text-blue-600 gap-[2px]">
+                    <GoPlus /> add new
+                  </button>
                 </div>
                 <div className="">
                   <p className="capitalize font-[500]">name sanaullah</p>
-                  <p className="text-gray-600 text-sm">Baresal, pord rood, Gournadi, Barisal District -8200, Bangladesh</p>
+                  <p className="text-gray-600 text-sm">
+                    Baresal, pord rood, Gournadi, Barisal District -8200,
+                    Bangladesh
+                  </p>
                   <p className="text-gray-600">01744584584</p>
                 </div>
               </div>
@@ -44,6 +53,15 @@ function Profile() {
           {/* recent order */}
           <h2 className="capitalize font-semibold text-xl">recent order</h2>
           <p> this section desgine after connect backend and upload product</p>
+        </div>
+        <div className="border-[4px] max-w-sm  sm:max-w-md md:max-w-[820px] mx-auto rounded-md p-1 sm:p-2  shadow-lg shadow-gray-400 my-10">
+          {/* inner border */}
+          {/* recent order */}
+          <h2 className="capitalize font-semibold text-xl">recent order</h2>
+          <p className=" bg-red-300 text-red-800">
+            {" "}
+            this section desgine after connect backend and upload product
+          </p>
         </div>
       </Container>
     </main>
