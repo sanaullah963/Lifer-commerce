17235003dcd11b75f9586f1c2a31a371282a5a39
sanaullah963 <allhera963@gmail.com>
2024-03-30 16:43:54 +0600

    modal grap value

--- client/src/components/profile/Modal.jsx
+++ client/src/components/profile/Modal.jsx
@@ -1,8 +1,30 @@
+"use client";
 import { districtArray } from "@/constant/data";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 
 function Modal({ closeModal }) {
+  const [district, setDistrict] = useState("");
+  const [upazila, setUpazila] = useState([]);
+  const [upazilaValue, setUpazilaValue] = useState("");
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("bangladesh");
+  const [number, setNumber] = useState("");
+  const [address, setAddress] = useState("");
+
+  // district handel and finding upazila array
+  const handelDistrict = (e) => {
+    setDistrict(e.target.value);
+    districtArray.map((i) => {
+      i.district == e.target.value && setUpazila(i.upazila);
+    });
+  };
+  // submit handel
+  const handelSubmit=()=>{
+
+    console.log(district,upazilaValue,name,number,country,address);
+    closeModal()
+  }
   return (
     <div className=" absolute h-auto top-0 right-0 left-0 bg-gray-200 px-2 md:px-10">
       {/* inner div */}
@@ -24,50 +46,100 @@ function Modal({ closeModal }) {
           <div className="">
             {/* name */}
             <div className="flex flex-col mb-3">
-              <label htmlFor="name" className="capitalize font-[500]">name*</label>
-              <input id="name" type="text" placeholder="Enter Full Name" className="outline-none border  hover:border-green-400 focus:border-green-400 rounded px-2 py-1" />
+              <label htmlFor="name" className="capitalize font-[500]">
+                name*
+              </label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Enter Full Name"
+                className="outline-none border  hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={(e)=>setName(e.target.value)}
+              />
             </div>
             {/* number */}
             <div className="flex flex-col mb-3">
-              <label htmlFor="number" className="capitalize font-[500]">number*</label>
-              <input id="number" type="text" placeholder="Enter number" className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1" />
+              <label htmlFor="number" className="capitalize font-[500]">
+                number*
+              </label>
+              <input
+                id="number"
+                type="text"
+                placeholder="Enter number"
+                className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={(e)=>setNumber(e.target.value)}
+              />
             </div>
             {/* country */}
             <div className="flex flex-col mb-3">
-              <label htmlFor="country" className="capitalize font-[500]">country*</label>
-              <select id="country" className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1">
-                  <option value="bangladesh" selected>Bangladesh</option>                        
+              <label htmlFor="country" className="capitalize font-[500]">
+                country*
+              </label>
+              <select
+                id="country"
+                className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={(e)=>setCountry(e.target.value)}
+              >
+                <option value="bangladesh" selected>
+                  Bangladesh
+                </option>
               </select>
             </div>
             {/* district */}
             <div className="flex flex-col mb-3">
-              <label htmlFor="district" className="capitalize font-[500]">district*</label>
-              <select id="district" className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1">
-                {
-                  districtArray.map((i,index)=>(
-                  <option key={index} value={i.district}>  {i.district}</option>
-                  ))
-                }
+              <label htmlFor="district" className="capitalize font-[500]">
+                district*
+              </label>
+              <select
+                id="district"
+                className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={handelDistrict}
+              >
+                <option selected>select district</option>
+                {districtArray.map((i, index) => (
+                  <option key={index} value={i.district}>
+                    {i.district}
+                  </option>
+                ))}
               </select>
             </div>
-              {/* upazila */}
-              <div className="flex flex-col mb-3">
-              <label htmlFor="upazila" className="capitalize font-[500]">upazila*</label>
-              <select id="upazila" className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1">
-                {
-                  districtArray.map((i,index)=>(
-                  <option key={index} value={i.district}>  {i.district}</option>
-                  ))
-                }
+            {/* upazila */}
+            <div className="flex flex-col mb-3">
+              <label htmlFor="upazila" className="capitalize font-[500]">
+                upazila*
+              </label>
+
+              <select
+                id="upazila"
+                className="outline-none border hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={(e)=>setUpazilaValue(e.target.value)}
+              >
+                {upazila.length <= 0 ? (
+                  <option selected>select district fist</option>
+                ) : (
+                  upazila.map((i, index) => (
+                    <option key={index} value={i}>{i}</option>))
+                )}
               </select>
             </div>
             {/* address */}
             <div className="flex flex-col mb-3">
-              <label htmlFor="address" className="capitalize font-[500]">address*</label>
-              <input id="address" type="text" placeholder=" Area , Road , House" className="outline-none border  hover:border-green-400 focus:border-green-400 rounded px-2 py-1" />
+              <label htmlFor="address" className="capitalize font-[500]">
+                address*
+              </label>
+              <input
+                id="address"
+                type="text"
+                placeholder=" Area , Road , House"
+                className="outline-none border  hover:border-green-400 focus:border-green-400 rounded px-2 py-1"
+                onChange={(e)=>setAddress(e.target.value)}
+              />
             </div>
             {/* submit button */}
-            <button className="w-full bg-green-400 hover:bg-green-500 py-1 capitalize font-[500] rounded text-xl mt-4">add address</button>
+            <button className="w-full bg-green-400 hover:bg-green-500 py-1 capitalize font-[500] rounded text-xl mt-4"
+            onClick={handelSubmit}>
+              add address
+            </button>
           </div>
         </div>
       </div>
