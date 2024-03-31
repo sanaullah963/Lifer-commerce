f732e0604af6bbd27c0893cf68ecb2ef7e9e5041
sanaullah963 <allhera963@gmail.com>
2024-03-31 19:47:49 +0600

    buy page added

--- client/src/components/BorderContainer.jsx
+++ client/src/components/BorderContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BorderContainer({children,className}) {
   return (
     <div className="p-2 sm:p-3">
-      <div className={` ${className} border-[4px] max-w-md  sm:max-w-[650px] md:max-w-[820px] lg:max-w-[1000px] mx-auto rounded-md p-1 sm:p-2 md:py-5 shadow-lg shadow-gray-400 my-10`}>{children}</div>
+      <div className={` ${className} border-[4px] max-w-md  sm:max-w-[650px] md:max-w-[820px] lg:max-w-[1000px] mx-auto rounded-md p-1 sm:p-2 md:py-5 shadow-lg shadow-gray-400 my-8`}>{children}</div>
     </div>
   )
 }
--- client/src/components/buy/buy.jsx
+++ client/src/components/buy/buy.jsx
@@ -6,12 +6,15 @@ import Link from "next/link";
 import imm from "@/image/proudct/p4.jpg";
 import BorderContainer from "../BorderContainer";
 async function Odder() {
+  const title =
+    "Vintage T9 Hair Cutting Machine Hair Trimmer Recharge Professional Cordless Hair Trimmer";
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <main>
       {/* first box */}
       <div className="">
         {/* address */}
-        <BorderContainer className=" ">
+        <BorderContainer className="  ">
           <div className="flex w-full justify-between">
             {/* left side */}
             <div className=" text-sm sm:text-lg flex-1 ">
@@ -45,44 +48,53 @@ async function Odder() {
           </div>
         </BorderContainer>
         {/* product & price */}
-        <BorderContainer className=" flex flex-col md:flex-row gap-1">
-          {/* product image,title,price,quentity, */}
-          <div className="md:w-2/3 w-full  bg-white ">
-            <div className=" flex gap-4">
-              {/* image  */}
-              <div className="h-20 w-20">
-                <Image
-                  src={imm}
-                  alt="product-Image"
-                  height={400}
-                  width={300}
-                  className="w-full"
-                />
+        <BorderContainer className=" flex flex-col my-0 md:flex-row gap-1">
+          {/* left-side */}
+          <div className="md:w-2/3 w-full">
+            {/* singel product */}
+            {arr.map((i, index) => (
+              <div
+                key={index}
+                className=" flex rounded-md bg-gray-100 gap-x-2 p-1 my-2"
+              >
+                {/* image  */}
+                <div className="w-[70px] rounded">
+                  <Image
+                    src={imm}
+                    alt="product-Image"
+                    height={400}
+                    width={400}
+                    className="w-full rounded"
+                  />
+                </div>
+                {/* price */}
+                <div className="flex-1 w-full">
+                  <p>{title}</p>
+                  <p className="text-primary text-end pe-1 md:pe-3">20*2=40</p>
+                </div>
               </div>
-              {/* price */}
-              <div className="w-full">
-                <p>{"product.title"}</p>
-                <p className="text-primary text-end">{"price"}</p>
-              </div>
-            </div>
+            ))}
           </div>
-          {/* total price and delivary charge */}
-          <div className=" bg-white shadow-lg rounded-md p-5 md:w-1/3 w-full ">
+          {/* ride-side */}
+          <div className=" bg-white shadow-lg rounded-md p-5 md:w-1/3 w-full mt-5 md:mt-0">
             {/* order header */}
-            <div className="flex justify-between">
-              <div className="">
-                <p>Total product :</p>
-                <p>price :</p>
-                <p>Delivery Fee :</p>
-                <p>Total Payment :</p>
-              </div>
-              <div className="text-primary flex flex-col items-end">
-                <p>{"quantity"}</p>
-                <p>{"product.newPrice * quantity"}</p>
-                <p>{"delivaryFee"}</p>
-                <p>{"product.newPrice * quantity + delivaryFee"}</p>
-              </div>
+
+            <div className="flex justify-between border-y capitalize">
+              <span>product quantity</span> <span>120</span>
             </div>
+            <div className="flex justify-between border-y capitalize">
+              <span>Total price</span> <span>120</span>
+            </div>
+            <div className="flex justify-between border-y capitalize">
+              <span>delivary fee</span> <span>120</span>
+            </div>
+            <div className="flex justify-between border-y capitalize">
+              <span>discount</span> <span>120</span>
+            </div>
+            <div className="flex justify-between border-y capitalize">
+              <span>total pay</span> <span>120</span>
+            </div>
+
             {/* place order button */}
             <button
               className="text-white bg-primary w-full h-10 mt-3 rounded-md"
