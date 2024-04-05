0171238ca099f56ecdb06f23b4363356bbfa0e20
sanaullah963 <allhera963@gmail.com>
2024-04-05 15:13:38 +0600

    remove multer

--- server/router/product.js
+++ server/router/product.js
@@ -2,14 +2,14 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 // multer config
-const upload = multer({
-  dest: "upload",
-});
+// const upload = multer({
+//   dest: "upload",
+// });
 // insart Product
-router.post("/insartProduct", upload.single("image"), async (req, res) => {
+router.post("/insartProduct",  async (req, res) => {
   const data = req.body;
   console.log(data);
-  res.send(req.file);
+  // res.send(req.file);
 });
 
 module.exports = router;
