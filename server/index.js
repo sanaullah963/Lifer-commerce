b2fc72ca682a9915308d967d87207c7e7f825fa0
sanaullah963 <allhera963@gmail.com>
2024-04-05 14:30:07 +0600

    server fixex 1

--- server/index.js
+++ server/index.js
@@ -1,35 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const product = require("./router/product");
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const product = require('./router/product')
-require('dotenv').config()
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/product", product);
 
+const PORT = process.env.PORT || 8000;
 
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/product',product)
-
-
-const PORT = process.env.PORT || 8000
-
-app.listen(PORT,()=>{
-  console.log('server is running in port :',PORT);
-})
-app.get('/',(req,res)=>{
-  res.send('Astag firullah')
-})
+app.listen(PORT, () => {
+  console.log("server is running in port :", PORT);
+});
+app.get("/", (req, res) => {
+  res.send("Astag firullah");
+});
 
 // connect mongoDb
-async function connect(){
+async function connect() {
   try {
-   await mongoose.connect(process.env.MONGODB_URI)
-      console.log('DB connected sucessfully');
-    
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected sucessfully");
   } catch (error) {
-    console.log('DB connection error');
+    console.log("DB connection error");
   }
 }
-connect()
+connect();
