6fccb99d613cbc88e96580f975c640b75e95c27a
sanaullah963 <allhera963@gmail.com>
2024-04-15 22:05:04 +0600

    test 2

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const user = require("./router/user");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true,optionsSuccessStatus: 200 }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use("/product", product);
 app.use('/uaer',user)
