391c0e17b379b4d6aa9477eb1b45d663be75d4c9
sanaullah963 <allhera963@gmail.com>
2024-04-15 21:51:54 +0600

    remove httpOnly false

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,7 +30,7 @@ const signupControl = async (req, res) => {
       const token = genaretToken(tokenInfo);
       // console.log(token);
       const exp = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000);
-      res.cookie("token",token,{httpOnly:true,expires: exp})
+      res.cookie("token",token,{expires: exp})
       res.send(newuser)
       
       
