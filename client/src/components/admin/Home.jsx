b7cdc5c22d8957faaaa5e768e928124c9d732b8e
sanaullah963 <allhera963@gmail.com>
2025-02-20 22:19:28 +0600

    admin dashbord update

--- client/src/components/admin/Home.jsx
+++ client/src/components/admin/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "../Container";
 import { useRouter } from "next/navigation";
 import { FaPlus } from "react-icons/fa";
+import Link from "next/link";
 
 function Home() {
   const router = useRouter()
@@ -11,20 +12,22 @@ function Home() {
     <Container>
       <div className="grid bg-slate-200 p-2 content-center gap-2 sm:gap-10 items-center grid-flow-row grid-cols-2 md:grid-cols-3">
         {/* total product */}
-        <div onClick={()=>router.push('/admin/product')} className="cursor-pointer bg-green-400 h-40 flex flex-col capitalize font-semibold rounded-md justify-center items-center">
+        <Link href={'/admin/product'} className="cursor-pointer bg-green-400 h-40 flex flex-col capitalize font-semibold rounded-md justify-center items-center" >
           <p className="text-white text-xl sm:text-2xl font-bold">total product</p>
           <p>500</p>
-        </div>
+        </Link>
         {/* order */}
-        <div onClick={()=>router.push('/admin/order')} className="cursor-pointer bg-green-400 h-40  flex flex-col capitalize font-semibold rounded-md justify-center items-center">
+        <Link href={'/admin/product'} className="cursor-pointer bg-green-400 h-40 flex flex-col capitalize font-semibold rounded-md justify-center items-center" >
           <p className="text-white text-xl sm:text-2xl font-bold">order</p>
           <p>500</p>
-        </div>
+          <p className="text-purple-600 ">new 25</p>
+        </Link>
         {/* add product */}
-        <div onClick={()=>router.push('/admin/insartproduct')} className="cursor-pointer bg-green-400 h-40  flex flex-col capitalize font-semibold rounded-md justify-center items-center">
+        <Link href={'/admin/product'} className="cursor-pointer bg-green-400 h-40 flex flex-col capitalize font-semibold rounded-md justify-center items-center" >
           <p className="text-white text-xl sm:text-2xl font-bold">add Product</p>
           <p><FaPlus /></p>
-        </div>
+        </Link>
+
       </div>
     </Container>
   );
